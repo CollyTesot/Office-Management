0b27c274dfa6dc785d1dbf69647ab7402b2f1b98
Khipcolly <collycherry@@gmail.com>
2022-06-23 22:19:50 +0300

    user-levels

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC0crP5KCEInk21IDCUIZ6czVKGmGQE7JM",
+  authDomain: "office-management-d323b.firebaseapp.com",
+  projectId: "office-management-d323b",
+  storageBucket: "office-management-d323b.appspot.com",
+  messagingSenderId: "82956101775",
+  appId: "1:82956101775:web:a6e5ef6305b50463d968fa",
+  measurementId: "G-QSX5GRDZZ2"
+};
+
+firebase.initializeApp(firebaseConfig);
